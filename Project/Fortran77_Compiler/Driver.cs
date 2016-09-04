35e11b7cfa655e41048652b3c1e23780c3259acf
BlueHorizon186 <bluehorizon186@gmail.com>
2016-09-03 19:36:38 -0500

    Added templates to begin developing the compiler.

---
+++ Project/Fortran77_Compiler/Driver.cs
@@ -0,0 +1,113 @@
+﻿/*
+  Fortran77 compiler - This class performs the lexical analysis, 
+  (a.k.a. scanning).
+  Copyright (C) 2016, ITESM CEM
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.IO;
+
+namespace Fortran77_Compiler
+{
+	public class Driver
+	{
+		// Current compiler version.
+		const string VERSION = "0.1";
+
+		//-----------------------------------------------------------
+
+		// Features the compiler includes.
+		static readonly string[] ReleaseIncludes = {
+			"Lexical Analysis"
+		};
+
+		//-----------------------------------------------------------
+
+		// Information printed whenever the compiler is used.
+		void PrintAppHeader() {
+			Console.WriteLine("Buttercup compiler, version " + VERSION);
+			Console.WriteLine("Copyright \u00A9 2016, ITESM CEM.");
+			Console.WriteLine("This program is free software; you may "
+				+ "redistribute it under the terms of");
+			Console.WriteLine("the GNU General Public License version 3 or "
+				+ "later.");
+			Console.WriteLine("This program has absolutely no warranty.\n");
+		}
+
+		void PrintReleaseIncludes() {
+			Console.WriteLine("Included in this release:");            
+			foreach (var phase in ReleaseIncludes) {
+				Console.WriteLine("   * " + phase);
+			}
+			Console.Write("\n");
+		}
+
+		//-----------------------------------------------------------
+
+		// Compiler's method to opening and interpreting the input file.
+		void Run(string[] args)
+		{
+			// Print some general information.
+			PrintAppHeader();
+			PrintReleaseIncludes();
+
+			// Check for input file and return an error if
+			// it was not provided.
+			if (args.Length != 1)
+			{
+				Console.Error.WriteLine(
+					"Please specify the name of the input file.");
+				Environment.Exit(1);
+			}
+
+			// Read the input file and parse the tokens, or
+			// return an error if the file was not found.
+			try
+			{
+				// Read the source file.
+				var inputPath = args[0];
+				var inputCode = File.ReadAllText(inputPath);
+
+				// Print header.
+				Console.WriteLine(String.Format(
+					"===== Tokens from: \"{0}\" =====", inputPath)
+				);
+
+				// Iterate over the file's tokens and print them
+				// accordingly.
+				var count = 1;
+				foreach (var token in new Scanner(inputCode).Start())
+				{
+					Console.WriteLine(String.Format("[{0}] {1}", 
+						count++, token)
+					);
+				}
+			}
+
+			catch (FileNotFoundException e)
+			{
+				Console.Error.WriteLine(e.Message);
+				Environment.Exit(1);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			// Begin the compiling process.
+			new Driver().Run(args);
+		}
+	}
+}
---
+++ Project/Fortran77_Compiler/Token.cs
@@ -0,0 +1,65 @@
+﻿/*
+  Fortran77 compiler - This class performs the lexical analysis, 
+  (a.k.a. scanning).
+  Copyright (C) 2016, ITESM CEM
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+
+namespace Fortran77_Compiler
+{
+	class Token
+	{
+		// Stores the actual text of this token.
+		readonly string lexeme;
+
+		// Stores which category this token belongs to.
+		readonly TokenCategory category;
+
+		// Stores which line or row this token was found in.
+		readonly int row;
+
+		// Stores which column this token was found in.
+		readonly int column;
+
+		// Properties to access the Token's fields.
+		public string Lexeme { get { return lexeme; }}
+		public TokenCategory Category { get { return category; }}
+		public int Row { get { return row; }}
+		public int Column { get { return column; }}
+
+		// Token Constructor
+		public Token(
+			string lexeme,
+			TokenCategory category,
+			int row,
+			int column)
+		{
+			this.lexeme = lexeme;
+			this.category = category;
+			this.row = row;
+			this.column = column;
+		}
+
+		// ToString method used to display the tokens accordingly.
+		public override string ToString()
+		{
+			return string.Format("{{{0}, \"{1}\", @({2}, {3})}}",
+				category, lexeme, row, column);
+		}
+	}
+}
+
