217de78d25a507937ae412159e4f98f3710e9314
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-13 23:25:24 -0600

    Started giving shape to the Semantic Analyzer. Next step: Begin with the Visit methods :)

--- Project/Fortran77_Compiler/SemanticAnalyzer.cs
+++ Project/Fortran77_Compiler/SemanticAnalyzer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fortran77_Compiler
 {
@@ -6,12 +7,34 @@ namespace Fortran77_Compiler
     {
         /*****************************************************************
          *  Missing Stuff to begin defining the Semantic Analyzer:
-         *  - Define the Type Enumeration.
-         *  - Define the TypeMapper according to the Type Enumeration.
-         *  - Create and define the Symbol Table class.
+         *  :) Define the Type Enumeration.
+         *  :) Define the TypeMapper according to the Type Enumeration.
+         *  :) Create and define the Symbol Table class.
+         * ***************************************************************
          *  - Instantiate the Symbol Table class as needed (Implementation)
          *  - Define all Visit methods.
          *  - Define all Visit methods for Operators.
          ****************************************************************/
+
+         //-----------------------------------------------------------
+         static readonly IDictionary<TokenCategory, Type> typeMapper =
+            new Dictionary<TokenCategory, Type>()
+            {
+                { TokenCategory.CHARACTER, Type.CHARACTER },
+                { TokenCategory.INTEGER, Type.INTEGER },
+                { TokenCategory.LOGICAL, Type.LOGICAL },
+                { TokenCategory.REAL, Type.REAL }
+            };
+
+        //-----------------------------------------------------------
+        public List<SymbolTable> Tables { get; private set; }
+
+        //-----------------------------------------------------------
+        public SemanticAnalyzer()
+        {
+            Tables = new List<SymbolTable>();
+        }
+
+        //-----------------------------------------------------------
     }
 }
