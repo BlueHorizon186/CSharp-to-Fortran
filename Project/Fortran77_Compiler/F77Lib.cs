51e9de9cec60dcf3a5b6257612dc2d8b1b8c7454
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-20 13:28:33 -0600

    Created runtime library with different Print methods.

---
+++ Project/Fortran77_Compiler/F77Lib.cs
@@ -0,0 +1,27 @@
+﻿namespace Fortran77_Compiler
+{
+    using System;
+
+    public class Utils
+    {
+        public static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void Print(int i)
+        {
+            Console.WriteLine(i.ToString());
+        }
+
+        public static void Print(double d)
+        {
+            Console.WriteLine(d.ToString());
+        }
+
+        public static void Print(bool b)
+        {
+            Console.WriteLine(b ? "true" : "false");
+        }
+    }
+}
