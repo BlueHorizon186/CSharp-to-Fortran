fff7c16218bdaff18c5c192bc527a1dbe95ed88e
BlueHorizon186 <bluehorizon186@gmail.com>
2016-11-20 18:12:01 -0600

    Assignment and Declarations done.

--- Project/Fortran77_Compiler/files/hello.f
+++ Project/Fortran77_Compiler/files/hello.f
@@ -1,8 +1,11 @@
 ! The world famous "Hello World!" program.
     
       program hello
+      integer i
+
+      i = 5
       
-      write(*, *) 'Hello World!', 5.3
+      write(*, *) 'Hello World!', i
       
       stop
       end
